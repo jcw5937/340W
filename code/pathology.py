c069f6427c14f4446f6280104728e3fd1cfbd3eb
Jake Willingham <jcw5937@psu.edu>
2025-11-07 14:30:41 -0500

    Cluster ready pathology

--- code/pathology.py
+++ code/pathology.py
@@ -5,10 +5,19 @@
 # Load module
 import pandas as pd
 import re
+import os
 
-# Read in the description data
-train = pd.read_csv('/content/drive/MyDrive/CBIS-DDSM/mass_case_description_train_set.csv')
-test = pd.read_csv('/content/drive/MyDrive/CBIS-DDSM/mass_case_description_test_set.csv')
+# Added for Cluster
+BASE_DIR = "/data/ds340w"
+WORK_DIR = os.path.join(BASE_DIR, "work")
+
+# Read in the description data - Changed for Cluster
+DATA_DIR  = os.path.join(BASE_DIR, "data")
+TRAIN_CSV = os.path.join(DATA_DIR, "mass_case_description_train_set.csv")
+TEST_CSV  = os.path.join(DATA_DIR, "mass_case_description_test_set.csv")
+
+train = pd.read_csv(TRAIN_CSV)
+test  = pd.read_csv(TEST_CSV)
 
 # Extract columns we need
 train_need = train[['patient_id', 'pathology', 'image file path', 'cropped image file path', 'ROI mask file path']]
@@ -23,5 +32,6 @@ merged_df['crop_name'] = merged_df['cropped image file path'].apply(lambda x: x.
 merged_df['ROI_name'] = merged_df['ROI mask file path'].apply(lambda x: x.split('/')[0])
 merged_df['full_name'] = merged_df['image file path'].apply(lambda x: x.split('/')[0])
 
-# Save the output
-merged_df.to_csv('all_mass_pathology.csv', index=False)
+# Save the output - Changed for Cluster
+merged_df.to_csv(os.path.join(WORK_DIR, 'all_mass_pathology.csv'), index=False)
+
